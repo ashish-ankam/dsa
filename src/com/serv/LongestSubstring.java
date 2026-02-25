9483b85cfe523da65936f9e7c8712f3def0bac21
ashish-ankam <ashishankam1999@gmail.com>
2026-02-25 21:08:32 +0530

    method

--- src/com/serv/LongestSubstring.java
+++ src/com/serv/LongestSubstring.java
@@ -1,10 +1,15 @@
 package com.serv;
 
 public class LongestSubstring {
+	
+	public static int LongestSub(String[] s) {
+		return 0;
+ 	   
+    }
 
 	public static void main(String[] args) {
 		
-
+       
 	}
 
 }
