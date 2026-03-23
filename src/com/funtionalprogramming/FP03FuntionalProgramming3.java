adb812c2688393c1c18d4d2fb14e41a031237325
ashish-ankam <ashishankam1999@gmail.com>
2026-03-23 23:40:57 +0530

    using system.out println method

---
+++ src/com/funtionalprogramming/FP03FuntionalProgramming3.java
@@ -0,0 +1,20 @@
+package com.funtionalprogramming;
+
+import java.util.List;
+
+public class FP03FuntionalProgramming3 {
+
+	
+	
+	public static void main(String[] args) {
+		
+
+		
+		 List<Integer> numbers = List.of(1,32,23423,65,454,343,66654,44,9);
+		 
+		 numbers.stream().forEach(System.out::println);
+		
+	}
+	
+
+}
