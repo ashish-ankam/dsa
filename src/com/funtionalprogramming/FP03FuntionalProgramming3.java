c34bdf1e6be1eef318fb588daf96971728c03a1c
ashish-ankam <ashishankam1999@gmail.com>
2026-03-29 23:21:46 +0530

    new commit

--- src/com/funtionalprogramming/FP03FuntionalProgramming3.java
+++ src/com/funtionalprogramming/FP03FuntionalProgramming3.java
@@ -13,6 +13,8 @@ public class FP03FuntionalProgramming3 {
 		 List<Integer> numbers = List.of(1,32,23423,65,454,343,66654,44,9);
 		 
 		 numbers.stream().forEach(System.out::println);
+		 
+		 
 		
 	}
 	
